33d67b0af15035dcad420ac236acba37cd3b6ce5
SongSunHo <tank0219@naver.com>
2016-11-30 11:50:27 +0900

    modify>>Hello.java:012456 by Song

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -3,6 +3,9 @@ package com.test.hello;
 public class Hello {
 	
 	public static void main(String[] args) {
-	  System.out.println("local Repository create Hello_jsp");	
+	  System.out.println("local Repository create Hello_jsp");
+	  // 012456:hotfix edit by Song
+	  System.out.println("hotfix edit/modify");
+	  // 012456:end
 	}
 }
